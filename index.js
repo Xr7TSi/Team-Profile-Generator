b3ce13b335099ae45d5af5dbfd253f5ce6543790
Jonathan Rein <jrein1296@gmail.com>
2021-04-20 13:23:08 -0400

    Adding inquirer questions.

--- index.js
+++ index.js
@@ -2,12 +2,90 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const { clear } = require('console');
 const markdown = require('./generateTeamProfile.js');
-const questions = [{
-  type: 'input',
-  message: 'Enter your project title:',
-  name: 'title',
-},
 
+const mgrQuestions = [{
+  type: 'input',
+  message: 'Enter Manager name:',
+  name: 'mgrName',
+},
+{
+  type: 'input',
+  message: 'Enter Manager employee ID:',
+  name: 'mgrID',
+},
+{
+type: 'input',
+message: 'Enter Manager email:',
+name: 'mgrEmail',
+},
+{
+type: 'input',
+message: 'Enter Manager office number:',
+name: 'mgrOffice',
+}];
+
+const eng1Questions = [{
+  type: 'input',
+  message: 'Enter Engineer 1 name:',
+  name: 'eng1Name',
+},
+{
+type: 'input',
+  message: 'Enter Engineer 1 employee ID:',
+  name: 'eng1ID',
+},
+{
+  type: 'input',
+    message: 'Enter Engineer 1 GitHub email:',
+    name: 'eng1Email',
+},
+{
+  type: 'input',
+    message: 'Enter Engineer 1 GitHub username:',
+    name: 'eng1GitHub',
+}];
+
+const eng2Questions = [{
+  type: 'input',
+  message: 'Enter Engineer 2 name:',
+  name: 'eng2Name',
+},
+{
+type: 'input',
+  message: 'Enter Engineer 2 employee ID:',
+  name: 'eng2ID',
+},
+{
+  type: 'input',
+    message: 'Enter Engineer 2 GitHub email:',
+    name: 'eng2Email',
+},
+{
+  type: 'input',
+    message: 'Enter Engineer 2 GitHub username:',
+    name: 'eng2GitHub',
+}];
+
+const internQuestions = [{
+  type: 'input',
+  message: 'Enter Intern name:',
+  name: 'internName',
+},
+{
+  type: 'input',
+  message: 'Enter Intern employee ID:',
+  name: 'internID', 
+},
+{
+  type: 'input',
+  message: 'Enter Intern email:',
+  name: 'internEmail', 
+},
+{
+  type: 'input',
+  message: 'Enter Intern school:',
+  name: 'internSchool', 
+}];
 
 
 function init() {
@@ -20,6 +98,10 @@ function init() {
 };
 
 init();
+
+// team manager’s name, employee ID, email address, and office number
+// engineer’s name, ID, email, and GitHub username
+// intern’s name, ID, email, and school
 
 
 
