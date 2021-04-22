1f5348b8ae428f31089948359daf746c84901b98
Jonathan Rein <jrein1296@gmail.com>
2021-04-22 15:35:28 -0400

    New renderHTML functino is working!

--- generateTeamProfile.js
+++ generateTeamProfile.js
@@ -1,6 +1,25 @@
-function genProfile() {
-    return "Hello world!";
+      
+function renderHTML() {
+    return `
+        <!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>\</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <h1>Here's some text!</h1>
+        
+        <script src="" async defer></script>
+    </body>
+</html>`;
   }
+  
 
 // function renderMgr() {
 
@@ -18,4 +37,7 @@ function genProfile() {
 
 // }
 
-module.exports = genProfile;
+
+module.exports = renderHTML;
+
+
