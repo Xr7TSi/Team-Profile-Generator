991c40b8f9ea84179bc01e057563b501debf03f7
Jonathan Rein <jrein1296@gmail.com>
2021-04-22 16:55:57 -0400

    Can hardcode manager name into html.

--- generateTeamProfile.js
+++ generateTeamProfile.js
@@ -1,10 +1,13 @@
-function renderHTML(data) {
-  
-  const Manager = require("./employeeClasses/manager.js");
+function getMgrName() {
+  name = "Fred";
+}
 
-  function renderMgr() {
-    return Manager;
-  }
+function renderMgrName() {
+  return name;
+}
+
+function renderHTML(data) {
+  getMgrName(data);
 
   return `
         <!DOCTYPE html>
@@ -13,30 +16,18 @@ function renderHTML(data) {
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>\</title>
+        <title>Team</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="">
     </head>
     <body>
         <h1>Here's some text!</h1> 
-        <h1> ${renderMgr(data.name)}</h1>
+        <h1> ${renderMgrName()}</h1>
 
         <script src="" async defer></script>
     </body>
 </html>`;
 }
-
-// function renderEng1() {
-
-// }
-
-// function renderEng2() {
-
-// }
-
-// function renderIntern() {
-
-// }
 
 module.exports = renderHTML;
